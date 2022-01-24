f28fc6319d68384ae88bc57f9166b03588b64168
art <immitto@gmail.com>
2022-01-24 19:44:19 +0300

    'deploy'

---
+++ precache-manifest.56ea4cd48490b3e3c613e6e6d4a53b58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d40caa1f1f81561932e9",
+    "url": "/ucs/css/app.7bc16cc7.css"
+  },
+  {
+    "revision": "be24414f2d58fccb524bb3172a294b62",
+    "url": "/ucs/index.html"
+  },
+  {
+    "revision": "d40caa1f1f81561932e9",
+    "url": "/ucs/js/app.e721e6dd.js"
+  },
+  {
+    "revision": "c5d28505127e80452749",
+    "url": "/ucs/js/chunk-vendors.4b685ec9.js"
+  },
+  {
+    "revision": "e24ec06977e65080473f0276b397ce34",
+    "url": "/ucs/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ucs/robots.txt"
+  }
+]);
